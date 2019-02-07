83900c6b08875e236c3148ec29bd867e6f4f99ad
twinh <twinhuang@qq.com>
2019-02-07 23:18:58 +0800

    refactor: nav更新为bs4

--- resources/views/votes/show-nav.php
+++ resources/views/votes/show-nav.php
@@ -1,12 +1,12 @@
 <div class="vote-nav">
   <ul class="nav tab-underline tab-underline-sm border-bottom">
-    <li class="<?= $req['sort'] == 'createTime' ? 'active border-primary' : '' ?>">
-      <a class="text-active-primary" href="<?= $url->full('votes/%s/show', $vote['id'], ['sort' => 'createTime']) ?>">
+    <li class="nav-item <?= $req['sort'] == 'createTime' ? 'active border-primary' : '' ?>">
+      <a class="nav-link text-active-primary" href="<?= $url->full('votes/%s/show', $vote['id'], ['sort' => 'createTime']) ?>">
         最新参赛
       </a>
     </li>
-    <li class="<?= $req['sort'] == 'voteCount' ? 'active border-primary' : '' ?>">
-      <a class="text-active-primary" href="<?= $url->full('votes/%s/show', $vote['id'], ['sort' => 'voteCount']) ?>">
+    <li class="nav-item <?= $req['sort'] == 'voteCount' ? 'active border-primary' : '' ?>">
+      <a class="nav-link text-active-primary" href="<?= $url->full('votes/%s/show', $vote['id'], ['sort' => 'voteCount']) ?>">
         最新排名
       </a>
     </li>
