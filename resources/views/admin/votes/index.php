a1181a8d0d38a3c160d4ad8b72757a476fb6fe4d
twinh <twinhuang@qq.com>
2019-02-22 12:12:34 +0800

    refactoring: form直接加载unparam

--- resources/views/admin/votes/index.php
+++ resources/views/admin/votes/index.php
@@ -70,7 +70,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['plugins/admin/js/data-table', 'form', 'jquery-unparam'], function () {
+  require(['plugins/admin/js/data-table', 'form'], function () {
     var $recordTable = $('.js-vote-table').dataTable({
       ajax: {
         url: $.queryUrl('admin/votes.json')
